ba33e80daae28ccad0ca10ae52170d37ce1ce84b
erick <erickgoncalves5@hotmail.com>
2023-08-17 21:09:57 -0300

    Commit inicial

---
+++ src/main/java/com/erick/gerenciadorbiblioteca/GerenciadorBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.erick.gerenciadorbiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorBibliotecaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/erick/gerenciadorbiblioteca/GerenciadorBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erick.gerenciadorbiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
